3953b5441aea7f29d0f838adc7d6c4a624cf5922
Calvin Metcalf <cmetcalf@appgeo.com>
2014-09-29 10:46:33 -0400

    get rid of 2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var through2 = require('through2');
+var through = require('through2');
 var derequrie = require('derequire');
 
 module.exports = function (file, opt1, opt2) {
   var data = '';
-  return through2(function (chunk, _, next) {
+  return through(function (chunk, _, next) {
     data += chunk.toString();
     next();
   }, function (next) {
